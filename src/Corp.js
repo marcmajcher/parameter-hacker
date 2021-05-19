27776fb4afc253a9131a4f99dbba97bf5044d426
Marc Majcher <majcher@gmail.com>
2021-05-19 01:20:50 -0500

    some layout

--- src/Corp.js
+++ src/Corp.js
@@ -2,22 +2,39 @@ import React from 'react';
 import './Corp.css';
 import CorpNode from './CorpNode';
 const corpMargin = 10;
+let info;
+const infoList = [];
 
 export default function Corp({ corp }) {
   console.log(corp);
 
   const shellStyle = {
-    width: `${corp.width + corpMargin*2}px`,
-    height: `${corp.height + corpMargin*2}px`,
+    width: `${corp.width + corpMargin * 2}px`,
+    height: `${corp.height + corpMargin * 2}px`,
   };
   const corpStyle = {
     width: `${corp.width}px`,
     height: `${corp.height}px`,
   };
 
+  function startDrag(e) {
+    info = { x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY };
+  }
+
+  function stopDrag(e) {
+    info.width = e.nativeEvent.offsetX - info.x;
+    info.height = e.nativeEvent.offsetY - info.y;
+    infoList.push({ ...info });
+  }
+
+  function dump() {
+    console.log(JSON.stringify(infoList));
+  }
+
   return <div className="corp-shell" style={shellStyle}>
-    <div className="corp-network" style={corpStyle}>
+    <div className="corp-network" onMouseDown={startDrag} onMouseUp={stopDrag} onClick={dump} style={corpStyle}>
       {corp.nodes.map(node => <CorpNode key={node.id} node={node} />)}
     </div>
   </div>;
 }
+
--- src/CorpNode.js
+++ src/CorpNode.js
@@ -2,16 +2,14 @@ import React from 'react';
 import './CorpNode.css';
 
 export default function CorpNode({ node }) {
-  const { id, pos, lock, type } = node;
-
-  const [x, y, w, h] = pos.split(',').map(n => parseInt(n, 10));
+  const { x, y, w, h, id, lock, type } = node;
 
   const nodeStyle = {
     left: x,
     top: y,
     width: `${w}px`,
     height: `${h}px`,
-    backgroundColor: type === 'open' ? '#339933' : '666633',
+    backgroundColor: type === 'open' ? '#001100' : '#999933',
   };
 
   return <div className="corp-node" style={nodeStyle}>Node [{id}] {lock ? 'LOCKED' : ''}</div>;
